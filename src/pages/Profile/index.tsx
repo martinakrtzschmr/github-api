98dd5d3f86e4ab31ca981d42af595be101ee97f3
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2022-03-09 00:57:27 -0300

    Organization of Repos column and content

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,29 +1,48 @@
 import React from 'react';
 
-import { Container, Main, LeftSide, RightSide } from './styles';
+import { Container, Main, LeftSide, RightSide, Repos } from './styles';
 import ProfileData from '../../components/ProfileData';
+import RepoCard from '../../components/RepoCard';
 
 const Profile: React.FC = () => {
-    return (
-        <Container>
-            <Main>
-                <LeftSide>
-                    <ProfileData
-                        username={'rhivia'}
-                        name={'Martin Alexander'}
-                        avatarUrl={'https://avatars.githubusercontent.com/u/17728136?v=4'}
-                        followers={100}
-                        following={100}
-                        company={'Lovely Stay'}
-                        location={'Portugal'}
-                        email={'alexander.martin64@gmail.com'}
-                        blog={'linkedin'}
-                    />
-                </LeftSide>
-                <RightSide></RightSide>
-            </Main>
-        </Container>
-    )
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData
+            username={'rhivia'}
+            name={'Martin Alexander'}
+            avatarUrl={'https://avatars.githubusercontent.com/u/17728136?v=4'}
+            followers={100}
+            following={100}
+            company={'Lovely Stay'}
+            location={'Portugal'}
+            email={'alexander.martin64@gmail.com'}
+            blog={'linkedin'}
+          />
+        </LeftSide>
+        <RightSide>
+          <Repos>
+            <h2>Repos</h2>
+
+            <div>
+              {[1, 2, 3].map(n => (
+                <RepoCard
+                  key={n}
+                  username={'martinak'}
+                  reponame={'github-api'}
+                  description={'User profiles using Github API.'}
+                  language={'Typescript'}
+                  stars={8}
+                  forks={4}
+                />
+              ))}
+            </div>
+          </Repos>
+        </RightSide>
+      </Main>
+    </Container>
+  )
 };
 
 export default Profile;
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,32 +1,58 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-    --horizontal-padding: 16px;
-    --vertical-padding: 24px;
+  --horizontal-padding: 16px;
+  --vertical-padding: 24px;
 
-    padding: var(--vertical-padding) var(--horizontal-padding);
+  padding: var(--vertical-padding) var(--horizontal-padding);
 `;
 
 export const Main = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 
-    margin: 0 auto;
-    max-width: 1280px;
+  margin: 0 auto;
+  max-width: 1280px;
 
-    @media (min-width: 768px) {
-        flex-direction: row;
-    }
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
 `;
 
 export const LeftSide = styled.div`
-    @media (min-width: 768px) {
-        width: 25%;
-    }
+  padding: 0 var(--horizontalPadding);
+
+  @media (min-width: 768px) {
+    width: 25%;
+  }
 `;
 
 export const RightSide = styled.div`
+  padding: 0 var(--horizontalPadding);
+  
+  @media (min-width: 768px) {
+    width: 75%;
+  }
+`;
+
+export const Repos = styled.div`
+  margin-top: var(--vertical-padding);
+
+  > h2 {
+    font-size: 16px;
+    font-weight: normal;
+  }
+  > div {
+    margin-top: 8px;
+
+    display: grid;
+    grid-gap: 16px;
+
+    grid-template-columns: 1fr;
+
     @media (min-width: 768px) {
-        width: 75%;
+      grid-template-columns: 1fr 1fr;
+      grid-auto-rows: minmax(min-content, max-content);
     }
+  }
 `;
